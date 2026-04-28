a4014ca595eb641a37958e977e73a55dcaf46425
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-04-28 02:57:55 -0700

    Chore: Added CreateFeedFollow query in feed_follows.sql.

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,16 @@
+-- name: CreateFeedFollow :many
+WITH inserted_feed_follow AS (
+INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+)
+RETURNING *
+)
+SELECT inserted_feed_follow.*, users.name as user_name, feeds.name as feed_name
+FROM inserted_feed_follow
+INNER JOIN users on users.id = inserted_feed_follow.user_id
+INNER JOIN feeds on feeds.id = inserted_feed_follow.feed_id;
