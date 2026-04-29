4e14b40c43b92cf3818beb8d088a0c74cd5b9299
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-04-29 09:40:20 -0700

    Chore: Updated table to have last_fetched_at column and created a MarkFeedFetched and GetNextFeedToFetch queries.

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -18,3 +18,13 @@ INNER JOIN users
 
 -- name: GetFeed :one
 SELECT * from feeds where feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
